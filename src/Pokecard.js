79553aa47cb85bbaced84c3c1ca0d2e0d4a2f063
Mohd Abid <meabid97@gmail.com>
2020-03-15 00:43:13 +0530

    Styling cards

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -14,10 +14,10 @@ class Pokecard extends Component {
    let imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.id}.png`;    
     return (
       <div className="Pokecard">
-        <h1 className="Pokecard-h3">{this.props.name}</h1>
+        <h1 className="Pokecard-title">{this.props.name}</h1>
         <img src={imgSrc} alt={this.props.name} />
-        <div>Type: {this.props.type}</div>
-        <div>EXP: {this.props.base_experience}</div>
+        <div className="Pokecard-data">Type: {this.props.type}</div>
+        <div className="Pokecard-data">EXP: {this.props.base_experience}</div>
       </div>
     );
   }
