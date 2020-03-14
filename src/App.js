a9eefde8606c8cbd85b301c9162fe2d871032520
Mohd Abid <meabid97@gmail.com>
2020-03-15 00:18:35 +0530

    Add Pokedex component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import Pokecard from './Pokecard';
-import './App.css';
+import React from "react";
+import Pokedex from "./Pokedex";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Pokedex</h1>
-      <Pokecard id={4} name="Charmander" type="fire" base_experience={62} />
+      <Pokedex />
     </div>
   );
 }
